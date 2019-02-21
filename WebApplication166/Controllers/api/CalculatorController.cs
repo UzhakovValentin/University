eeb7f2aa2487b91c0ade71d573924a8e8ec259a9
Unknown <yuz-valya@yandex.ru>
2019-02-21 17:08:05 +0300

    project init

---
+++ WebApplication166/Controllers/api/CalculatorController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication166.Models;
+
+namespace WebApplication166.Controllers.api
+{
+    public class CalculatorController : ApiController
+    {
+        [HttpPost]
+        public CalculatorResponse Calculate
+                            (CalculatorRequest request)
+        {
+            decimal result;
+
+            switch (request.Action)
+            {
+                case "+":
+                    result = request.DigA + request.DigB;
+                    break;
+                case "-":
+                    result = request.DigA - request.DigB;
+                    break;
+                case "*":
+                    result = request.DigA * request.DigB;
+                    break;
+                case "/":
+                    result = request.DigA / request.DigB;
+                    break;
+                case "^":
+                    result = Pow(request.DigA, request.DigB);
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            return new CalculatorResponse { Result = result };
+        }
+
+        private decimal Pow(decimal a, decimal b)
+        {
+            decimal temp = 1;
+            for (int i = 0; i < b; i++)
+            {
+                temp *= a;
+            }
+            return temp;
+        }
+    }
+}
---
+++ WebApplication166/Models/CalculatorRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication166.Models
+{
+    public class CalculatorRequest
+    {
+        public decimal DigA { get; set; }
+        public decimal DigB { get; set; }
+        public string Action { get; set; }
+    }
+}
