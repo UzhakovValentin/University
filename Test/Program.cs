0efb037cc6a3006833efd6c9280b9bf1e4f20b31
Uzhakov Valentine <yuz-valya@yandex.ru>
2019-03-14 16:39:20 +0300

    APIclient

---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication166.Models;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = ApiClient.Post<CalculatorRequest, CalculatorResponse>(new CalculatorRequest { DigA = 5, DigB = 10, Action = "+" }, "http://localhost:2345/api/calculator/calculate");
+            Console.WriteLine(obj.Result);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Test/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Test
+{
+    public static class Serializer
+    {
+        public static string ToJson<T>(this T instance)
+        {
+            return JsonConvert.SerializeObject(instance);
+        }
+
+        public static T JsonParseAs<T>(this string source)
+        {
+            return (T)JsonConvert.DeserializeObject(source, typeof(T));
+        }
+    }
+}
